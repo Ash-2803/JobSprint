79cce6036353e37d809498514b31ee6f65a88575
Ash-2803 <tyagiayush326@gmail.com>
2025-12-29 17:57:32 +0530

    Add Model Changes done

--- job-sprint/src/Pages/Feed/Feeds.jsx
+++ job-sprint/src/Pages/Feed/Feeds.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useState } from 'react'
 import Card from '../../components/Card/Card'
 import ProfileCard from '../../components/Profile Card/ProfileCard'
 import MyImage from "../../assets/My_image.jpeg";
@@ -7,65 +8,72 @@ import AddPhotoAlternateSharpIcon from '@mui/icons-material/AddPhotoAlternateSha
 import DescriptionSharpIcon from '@mui/icons-material/DescriptionSharp';
 import Advertisement from '../../components/Advertisement/Advertisement';
 import Post from '../../components/Posts/Post';
+import Model from '../../components/Models/Model';
+import Addmodel from '../../components/AddModel/Addmodel';
 
 
 const Feeds = () => {
+
+  const [addPostmodel, setAddPostmodel] = useState(false);
+  const handleOpenPostmodel = () => {
+    setAddPostmodel(false);
+  }
   return (
     <div className='px-4 xl:px-50 py-9 flex gap-5 w-full mt-5 bg-gray-100'>
       {/* left side  */}
       <div className=' w-[21%] sm:block sm:w-[23%] hidden py-5'>
-      <div className='h-fit'>
-      <ProfileCard/>
-      </div>
-      <div className='w-full my-5'>
-      <Card padding={1}>
-        {/* <div className='font-semibold text-lg mb-3'>Skills</div> */}
-        <div className=' w-full flex justify-between'>
-          <div>Profile Viewers</div>
-          <div className='text-orange-400'>20</div>
-          </div>
-          <div className=' w-full flex justify-between'>
-          <div>Post Impression</div>
-          <div className='text-orange-400'>90</div>
+        <div className='h-fit'>
+          <ProfileCard />
         </div>
-      </Card>
-      </div>
+        <div className='w-full my-5'>
+          <Card padding={1}>
+            {/* <div className='font-semibold text-lg mb-3'>Skills</div> */}
+            <div className=' w-full flex justify-between'>
+              <div>Profile Viewers</div>
+              <div className='text-orange-400'>20</div>
+            </div>
+            <div className=' w-full flex justify-between'>
+              <div>Post Impression</div>
+              <div className='text-orange-400'>90</div>
+            </div>
+          </Card>
+        </div>
       </div>
       {/* middle side */}
-      
-    <div className='w-full py-5 sm:w-[50%]'>
-      {/* post box */}
-      <div>
-        <Card padding={1}>
-        <div className='flex gap-2 items-center'>
-          <img src={MyImage} alt="My Image" className='rounded-4xl w-13 h-13 border-2 border-white cursor-pointer'  />
-          <div className='w-full border py-3 px-3 rounded-3xl cursor-pointer hover:bg-gray-100'>Post Something You will</div>
-        </div>
-        <div className='w-full flex mt-3'>
-          <div className='w-1/3 flex justify-center items-center gap-2 cursor-pointer hover:bg-gray-100 py-2 rounded-md'>
-            <PlayCircleFilledSharpIcon className="text-green-500" />
-            <div className='text-sm'>Video</div>    
-          </div>
-          <div className='w-1/3 flex justify-center items-center gap-2 cursor-pointer hover:bg-gray-100 py-2 rounded-md'>
-            <AddPhotoAlternateSharpIcon className="text-blue-500" />
-            <div className='text-sm'>Photo</div>    
-          </div>
-          <div className='w-1/3 flex justify-center items-center gap-2 cursor-pointer hover:bg-gray-100 py-2 rounded-md'>
-            <DescriptionSharpIcon className="text-red-600" />
-            <div className='text-sm'>Write Article</div>    
-          </div>
+
+      <div className='w-full py-5 sm:w-[50%]'>
+        {/* post box */}
+        <div>
+          <Card padding={1}>
+            <div className='flex gap-2 items-center'>
+              <img src={MyImage} alt="My Image" className='rounded-full w-15 h-15 border-2 border-white cursor-pointer' />
+              <div onClick={() => setAddPostmodel(true)} className='w-full border py-3 px-3 rounded-3xl cursor-pointer hover:bg-gray-100'>Post Something</div>
+            </div>
+            <div className='w-full flex mt-3'>
+              <div onClick={() => setAddPostmodel(true)} className='w-1/3 flex justify-center items-center gap-2 cursor-pointer hover:bg-gray-100 py-2 rounded-md'>
+                <PlayCircleFilledSharpIcon className="text-green-500" />
+                <div className='text-sm'>Video</div>
+              </div>
+              <div onClick={() => setAddPostmodel(true)} className='w-1/3 flex justify-center items-center gap-2 cursor-pointer hover:bg-gray-100 py-2 rounded-md'>
+                <AddPhotoAlternateSharpIcon className="text-blue-500" />
+                <div className='text-sm'>Photo</div>
+              </div>
+              <div className='w-1/3 flex justify-center items-center gap-2 cursor-pointer hover:bg-gray-100 py-2 rounded-md'>
+                <DescriptionSharpIcon className="text-red-600" />
+                <div className='text-sm'>Write Article</div>
+              </div>
+            </div>
+
+          </Card>
         </div>
 
-        </Card>
+        <div className='border b-1 border-gray-400 w-full my-5' />
+        <div className='w-full flex flex-col gap-5 '>
+          <Post />
+          <Post />
+          <Post />
+        </div>
       </div>
-      
-      <div className='border b-1 border-gray-400 w-full my-5'/>
-      <div className='w-full flex flex-col gap-5 '>
-      <Post/>
-      <Post/>
-      <Post/>
-      </div>
-    </div>
       {/* right side  */}
       <div className='w-[26%] py-5 hidden md:block'>
         <div>
@@ -76,12 +84,15 @@ const Feeds = () => {
               <div className='text-md'>JobSprint launches new feature</div>
               <div className='text-xs text-gray-400'>JobSprint has launched a new feature to help you connect with recruiters more easily.</div>
             </div>
-            </Card>
+          </Card>
         </div>
-       <div className='my-5 sticky top-19'>
-        <Advertisement/>
-       </div>
+        <div className='my-5 sticky top-19'>
+          <Advertisement />
+        </div>
       </div>
+      {addPostmodel && <Model closeModel = {handleOpenPostmodel} title="Add Post">
+        <Addmodel />
+      </Model>}
     </div>
   )
 }
